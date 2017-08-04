6bcbd4bc66d6ff8cd6583833292ad306ad813ff3
Stephen Bahr <sbahr@bu.edu>
2017-08-04 18:14:13 -0400

    - Initial commit of website.

---
+++ js/library.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  
+  init_document_ready();
+
+});
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - 
+/* - - - - - - - - - - - - - - - - - - - - - - - - -
+ * Anything that should be run as soon as DOM is 
+ * loaded.
+ * - - - - - - - - - - - - - - - - - - - - - - - - -
+ */
+function init_document_ready() {
+
+  // create new method calls
+
+}
+
